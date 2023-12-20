c838b604fe95f2ac4535efcf8d22a709f25289c8
KevinClair <kevinclair@apache.org>
2023-12-21 00:39:07 +0800

    init

---
+++ src/main/java/com/github/kevin/raft/RaftApplication.java
@@ -0,0 +1,13 @@
+package com.github.kevin.raft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kevin/raft/RaftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kevin.raft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
