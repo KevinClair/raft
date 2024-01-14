4eabae2e29ad101904c3e2025dcdfbaf0fde1a6f
KevinClair <kevinclair@apache.org>
2024-01-14 20:48:19 +0800

    序列化方法

---
+++ src/main/java/com/github/kevin/raft/netty/common/codec/Serializer.java
@@ -0,0 +1,41 @@
+package com.github.kevin.raft.netty.common.codec;
+
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
+import com.github.kevin.raft.netty.common.entity.Message;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Objects;
+
+/**
+ * 序列化和反序列化
+ */
+public class Serializer {
+
+    public static byte[] serialize(final Object object) throws Exception {
+        Hessian2Output hessianOutput = null;
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();) {
+            hessianOutput = new Hessian2Output(outputStream);
+            hessianOutput.writeObject(object);
+            hessianOutput.flush();
+            return outputStream.toByteArray();
+        } finally {
+            if (Objects.nonNull(hessianOutput)) {
+                hessianOutput.close();
+            }
+        }
+    }
+
+    public static Message deserialize(final byte[] data) throws Exception {
+        Hessian2Input hessian2Input = null;
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data);) {
+            hessian2Input = new Hessian2Input(inputStream);
+            return (Message) hessian2Input.readObject();
+        } finally {
+            if (Objects.nonNull(hessian2Input)){
+                hessian2Input.close();
+            }
+        }
+    }
+}
