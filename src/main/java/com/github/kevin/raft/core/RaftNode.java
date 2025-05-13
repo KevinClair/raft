d940db2a5701cc55f3986e291fd62e5a2feaf5f9
KevinClair <kevinclair@apache.org>
2025-05-13 19:37:11 +0800

    初始化Raft节点

---
+++ src/main/java/com/github/kevin/raft/core/RaftNode.java
@@ -0,0 +1,76 @@
+package com.github.kevin.raft.core;
+
+import com.github.kevin.raft.common.ServerState;
+import com.github.kevin.raft.core.thread.ElectionThread;
+import com.github.kevin.raft.core.thread.HeartBeatThread;
+import com.github.kevin.raft.entity.PersistentState;
+import com.github.kevin.raft.netty.client.NettyClient;
+import com.github.kevin.raft.netty.server.NettyServer;
+import com.github.kevin.raft.utils.NameThreadPoolFactory;
+
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
+ * 节点
+ */
+public class RaftNode {
+
+    /**
+     * 当前服务器节点的地址
+     */
+    private final String address;
+
+    /**
+     * 其他节点的地址
+     */
+    private final List<String> otherAddresses;
+
+    /**
+     * 一致性模块
+     */
+    private final ConsensusModule consensusModule;
+
+    /**
+     * RPC客户端
+     */
+//    private final RaftRpcClient rpcClient;
+
+    /**
+     * 持久化状态
+     */
+    private final PersistentState persistentState;
+
+    /**
+     * 节点当前状态，初始化状态为follower
+     *
+     * @see ServerState
+     */
+    public volatile ServerState state = ServerState.FOLLOWER;
+
+    private ScheduledExecutorService electionScheduledExecutorService = new ScheduledThreadPoolExecutor(1, new NameThreadPoolFactory("election"));
+    private ScheduledExecutorService heartbeatScheduledExecutorService = new ScheduledThreadPoolExecutor(1, new NameThreadPoolFactory("heartbeat"));
+
+    public RaftNode(String address, List<String> otherAddresses, Integer port) {
+        this.address = address;
+        this.otherAddresses = otherAddresses;
+        this.consensusModule = new ConsensusModule(this);
+        // 启动服务端
+        new NettyServer(port);
+        // 延迟3秒启动客户端
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        otherAddresses.forEach(otherAddress -> {
+            RaftRpcClientContainer.getInstance().addRpcClient(otherAddress, new RaftRpcClient(new NettyClient(otherAddress).getChannel()));
+        });
+        this.persistentState = new PersistentState();
+        // 开启选举线程
+        electionScheduledExecutorService.scheduleWithFixedDelay(new ElectionThread(), 3000, 500, java.util.concurrent.TimeUnit.MILLISECONDS);
+        // 开启心跳线程
+        heartbeatScheduledExecutorService.scheduleAtFixedRate(new HeartBeatThread(), 0, 500, java.util.concurrent.TimeUnit.MILLISECONDS);
+    }
+}
