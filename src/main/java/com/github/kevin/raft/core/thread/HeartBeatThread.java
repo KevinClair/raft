46a8cacefa31b62e42819a2614ba9c881ad5ce24
KevinClair <kevinclair@apache.org>
2025-05-13 15:22:48 +0800

    选取线程和心跳线程

---
+++ src/main/java/com/github/kevin/raft/core/thread/HeartBeatThread.java
@@ -0,0 +1,13 @@
+package com.github.kevin.raft.core.thread;
+
+/**
+ * 心跳线程
+ *
+ * @author KevinClair
+ **/
+public class HeartBeatThread implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
