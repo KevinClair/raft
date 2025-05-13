f6b4a651390db1a1781fa34f41255cd96db0bcaa
KevinClair <kevinclair@apache.org>
2025-05-13 16:10:39 +0800

    调整配置文件属性值

--- src/main/java/com/github/kevin/raft/configuration/RaftConfigurationProperties.java
+++ src/main/java/com/github/kevin/raft/configuration/RaftConfigurationProperties.java
@@ -3,14 +3,22 @@ package com.github.kevin.raft.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.Arrays;
-import java.util.List;
-
 @ConfigurationProperties(prefix = "raft")
 @Data
 public class RaftConfigurationProperties {
 
-    private List<String> address = Arrays.asList("localhost:27015", "localhost:27016", "localhost:27017", "localhost:27018", "localhost:27019");
+    /**
+     * 本机Ip
+     */
+    private String selfAddress;
 
+    /**
+     * 启动端口
+     */
     private Integer port;
+
+    /**
+     * 除本机之外的Ip地址
+     */
+    private String address = "localhost:27015,localhost:27016,localhost:27017,localhost:27018,localhost:27019";
 }
