61f2f4fda3e342f02efbe563d5964e091aaf0382
KevinClair <kevinclair@apache.org>
2025-05-17 21:53:41 +0800

    添加心跳检测

---
+++ src/main/java/com/github/kevin/raft/netty/client/HeartBeatClientHandler.java
@@ -0,0 +1,37 @@
+package com.github.kevin.raft.netty.client;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 心跳响应处理器
+ *
+ * @author KevinClair
+ **/
+@ChannelHandler.Sharable
+public class HeartBeatClientHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HeartBeatClientHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            if (event.state() == IdleState.WRITER_IDLE) {
+                // 处理写超时
+            }
+            return;
+        }
+        super.userEventTriggered(ctx, evt);
+    }
+}
---
+++ src/main/java/com/github/kevin/raft/netty/server/HeartBeatServerHandler.java
@@ -0,0 +1,55 @@
+package com.github.kevin.raft.netty.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 心跳请求处理器
+ *
+ * @author KevinClair
+ **/
+@ChannelHandler.Sharable
+public class HeartBeatServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HeartBeatServerHandler.class);
+
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) {
+
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            // 判断是否为读事件
+            IdleStateEvent event = (IdleStateEvent) evt;
+            if (event.state() == IdleState.READER_IDLE) {
+                Channel channel = ctx.channel();
+                logger.warn("Heartbeat check, it's more than two minutes since the last heartbeat,channel {} has lost connection.", channel.id());
+                ctx.close();
+            }
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+}
