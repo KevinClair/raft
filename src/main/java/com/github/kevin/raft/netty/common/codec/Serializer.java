b8e294fffe2227fbc267e62d968e39ca6dbc8918
KevinClair <kevinclair@apache.org>
2025-06-01 23:23:42 +0800

    调整序列化工具类

--- src/main/java/com/github/kevin/raft/netty/common/codec/Serializer.java
+++ src/main/java/com/github/kevin/raft/netty/common/codec/Serializer.java
@@ -2,7 +2,6 @@ package com.github.kevin.raft.netty.common.codec;
 
 import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.Hessian2Output;
-import com.github.kevin.raft.netty.common.entity.Request;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -28,11 +27,11 @@ public class Serializer {
         }
     }
 
-    public static Request deserialize(final byte[] data) throws Exception {
+    public static Object deserialize(final byte[] data) throws Exception {
         Hessian2Input hessian2Input = null;
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data);) {
             hessian2Input = new Hessian2Input(inputStream);
-            return (Request) hessian2Input.readObject();
+            return hessian2Input.readObject();
         } finally {
             if (Objects.nonNull(hessian2Input)){
                 hessian2Input.close();
