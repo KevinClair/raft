5f0c730402485a1a9b2520e1b6851aa1be86ab07
KevinClair <kevinclair@apache.org>
2025-06-02 17:56:56 +0800

    完善Raft领导者选举和日志复制逻辑

--- src/main/java/com/github/kevin/raft/core/ConsensusModule.java
+++ src/main/java/com/github/kevin/raft/core/ConsensusModule.java
@@ -1,15 +1,196 @@
 package com.github.kevin.raft.core;
 
+import com.github.kevin.raft.common.ServerStatus;
+import com.github.kevin.raft.dto.AppendEntriesRequest;
+import com.github.kevin.raft.dto.AppendEntriesResponse;
+import com.github.kevin.raft.dto.RequestVoteRequest;
+import com.github.kevin.raft.dto.RequestVoteResponse;
+import com.github.kevin.raft.entity.LogEntry;
+import io.netty.util.internal.StringUtil;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 共识模块
  */
 @Data
 @RequiredArgsConstructor
+@Slf4j
 public class ConsensusModule {
 
     private final RaftNode raftNode;
 
+    // 追加日志Lock
+    public final ReentrantLock appendLock = new ReentrantLock();
+    // 投票Lock
+    public final ReentrantLock voteLock = new ReentrantLock();
+
+    /**
+     * 处理追加日志请求
+     *
+     * @param appendEntriesRequest 附加日志数据
+     * @return AppendEntriesResponse 响应结果
+     */
+    public AppendEntriesResponse handleAppendEntries(AppendEntriesRequest appendEntriesRequest) {
+
+        // 先初始化一个AppendEntriesResponse
+        AppendEntriesResponse appendEntriesResponse = AppendEntriesResponse.fail();
+
+        try {
+            // 尝试加锁
+            if (!appendLock.tryLock(50, TimeUnit.MILLISECONDS)) {
+                log.warn("AppendEntries lock is busy, returning failure response.");
+                return appendEntriesResponse; // 返回失败响应
+            }
+
+            // 设置当前任期
+            appendEntriesResponse.setTerm(raftNode.getPersistentState().getCurrentTerm());
+            // 判断请求参数中的任期是否小于当前任期
+            if (appendEntriesRequest.getCurrentTerm() < raftNode.getPersistentState().getCurrentTerm()) {
+                log.warn("Received AppendEntries with outdated term: {}", appendEntriesRequest.getCurrentTerm());
+                return appendEntriesResponse; // 返回失败响应
+            }
+
+            long currentTimeMillis = System.currentTimeMillis();
+            raftNode.setPreHeartBeatTime(currentTimeMillis);
+            raftNode.setPreElectionTime(currentTimeMillis);
+            raftNode.setLeaderId(appendEntriesRequest.getLeaderId());
+
+            // 如果请求的任期大于当前任期，更新当前节点状态为Follower
+            if (appendEntriesRequest.getCurrentTerm() >= raftNode.getPersistentState().getCurrentTerm()) {
+                log.info("Updating current term from {} to {}", raftNode.getPersistentState().getCurrentTerm(), appendEntriesRequest.getCurrentTerm());
+                raftNode.setStatus(ServerStatus.FOLLOWER);
+            }
+            // 更新当前节点的任期
+            raftNode.getPersistentState().setCurrentTerm(appendEntriesRequest.getCurrentTerm());
+
+            // 如果追加日志请求中没有日志条目，为心跳请求
+            if (CollectionUtils.isEmpty(appendEntriesRequest.getEntries())) {
+                log.info("Received heartbeat from leader: {}", appendEntriesRequest.getLeaderId());
+                // 处理心跳请求，更新状态机等
+                // 如果leaderCommit大于当前commitIndex，则更新commitIndex
+                if (appendEntriesRequest.getLeaderCommit() > raftNode.getVolatileState().getCommitIndex()) {
+                    long newCommitIndex = Math.min(appendEntriesRequest.getLeaderCommit(), raftNode.getVolatileState().getLastApplied());
+                    raftNode.getVolatileState().setCommitIndex(newCommitIndex);
+                    raftNode.getVolatileState().setLastApplied(newCommitIndex);
+                }
+                // 提交之前未提交的日志条目
+                long nextCommit = raftNode.getVolatileState().getCommitIndex() + 1;
+                while (nextCommit <= raftNode.getVolatileState().getCommitIndex()) {
+                    // 提交日志条目到状态机
+                    raftNode.getStateMachine().apply(raftNode.getLogModule().read(nextCommit));
+                    nextCommit++;
+                }
+                return AppendEntriesResponse.builder().term(raftNode.getPersistentState().getCurrentTerm()).success(true).build(); // 返回成功响应
+            }
+            // 处理真实的日志条目
+            // 第一次
+            if (raftNode.getLogModule().getLastIndex() != 0 && appendEntriesRequest.getPreviousLogIndex() != 0) {
+                LogEntry logEntry = raftNode.getLogModule().read(appendEntriesRequest.getPreviousLogIndex());
+                if (Objects.nonNull(logEntry)) {
+                    // 如果日志在 prevLogIndex 位置处的日志条目的任期号和 prevLogTerm 不匹配，则返回 false
+                    // 需要减小 nextIndex 重试.
+                    if (logEntry.getTerm() != appendEntriesRequest.getPreviousLogTerm()) {
+                        // 打印错误日志
+                        log.error(
+                                "Log term mismatch: expected {}, got {} at index {}",
+                                appendEntriesRequest.getPreviousLogTerm(),
+                                logEntry.getTerm(),
+                                appendEntriesRequest.getPreviousLogIndex()
+                        );
+                        return appendEntriesResponse;
+                    }
+                } else {
+                    // index 不对, 需要递减 nextIndex 重试.
+                    return appendEntriesResponse;
+                }
+            }
+
+            // 如果已经存在的日志条目和新的产生冲突（索引值相同但是任期号不同），删除这一条和之后所有的
+            LogEntry existLog = raftNode.getLogModule().read(appendEntriesRequest.getPreviousLogIndex() + 1);
+            if (existLog != null && existLog.getTerm() != appendEntriesRequest.getEntries().get(0).getTerm()) {
+                // 删除这一条和之后所有的, 然后写入日志和状态机.
+                raftNode.getLogModule().removeOnStartIndex(appendEntriesRequest.getPreviousLogIndex() + 1);
+            } else if (existLog != null) {
+                // 已经有日志了, 不能重复写入.
+                appendEntriesResponse.setSuccess(true);
+                return appendEntriesResponse;
+            }
+
+            // 写进日志
+            for (LogEntry entry : appendEntriesRequest.getEntries()) {
+                raftNode.getLogModule().write(entry);
+                appendEntriesResponse.setSuccess(true);
+            }
+
+            //如果 leaderCommit > commitIndex，令 commitIndex 等于 leaderCommit 和 新日志条目索引值中较小的一个
+            if (appendEntriesRequest.getLeaderCommit() > raftNode.getVolatileState().getCommitIndex()) {
+                Long commitIndex = Math.min(appendEntriesRequest.getLeaderCommit(), raftNode.getLogModule().getLastIndex());
+                raftNode.getVolatileState().setCommitIndex(commitIndex);
+                raftNode.getVolatileState().setLastApplied(commitIndex);
+            }
+
+            // 下一个需要提交的日志的索引（如有）
+            for (long nextCommit = raftNode.getVolatileState().getCommitIndex() + 1; nextCommit <= raftNode.getVolatileState().getCommitIndex(); nextCommit++) {
+                // 提交之前的日志
+                raftNode.getStateMachine().apply(raftNode.getLogModule().read(nextCommit));
+            }
+
+            appendEntriesResponse.setTerm(raftNode.getPersistentState().getCurrentTerm());
+            raftNode.setStatus(ServerStatus.FOLLOWER);
+            return appendEntriesResponse;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            appendLock.unlock();
+        }
+    }
+
+    public RequestVoteResponse handleRequestVote(RequestVoteRequest requestVoteRequest) {
+        RequestVoteResponse.RequestVoteResponseBuilder builder = RequestVoteResponse.builder();
+        try {
+            if (!voteLock.tryLock(50, TimeUnit.MILLISECONDS)) {
+                return builder.term(raftNode.getPersistentState().getCurrentTerm()).voteGranted(false).build();
+            }
+
+            // 对方任期没有自己新
+            if (requestVoteRequest.getTerm() < raftNode.getPersistentState().getCurrentTerm()) {
+                return builder.term(raftNode.getPersistentState().getCurrentTerm()).voteGranted(false).build();
+            }
+
+            // (当前节点并没有投票 或者 已经投票过了且是对方节点) && 对方日志和自己一样新
+            log.info("node {} current vote for [{}], param candidateId : {}", raftNode.getAddress(), raftNode.getPersistentState().getVotedFor(), requestVoteRequest.getCandidateId());
+            log.info("node {} current term {}, peer term : {}", raftNode.getAddress(), raftNode.getPersistentState().getCurrentTerm(), requestVoteRequest.getTerm());
+
+            if ((StringUtil.isNullOrEmpty(raftNode.getPersistentState().getVotedFor()) || raftNode.getPersistentState().getVotedFor().equals(requestVoteRequest.getCandidateId()))) {
+
+                if (raftNode.getLogModule().getLast() != null) {
+                    // 对方没有自己新
+                    if (raftNode.getLogModule().getLast().getTerm() > requestVoteRequest.getLastLogTerm() || raftNode.getLogModule().getLastIndex() > requestVoteRequest.getLastLogIndex()) {
+                        return RequestVoteResponse.fail();
+                    }
+                }
+
+                // 切换状态
+                raftNode.setStatus(ServerStatus.FOLLOWER);
+                // 更新
+                raftNode.setLeaderId(requestVoteRequest.getCandidateId());
+                raftNode.getPersistentState().setCurrentTerm(requestVoteRequest.getTerm());
+                raftNode.getPersistentState().setVotedFor(requestVoteRequest.getCandidateId());
+                // 返回成功
+                return builder.term(raftNode.getPersistentState().getCurrentTerm()).voteGranted(true).build();
+            }
+            return builder.term(raftNode.getPersistentState().getCurrentTerm()).voteGranted(false).build();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            voteLock.unlock();
+        }
+    }
 }
--- src/main/java/com/github/kevin/raft/core/RaftNode.java
+++ src/main/java/com/github/kevin/raft/core/RaftNode.java
@@ -2,23 +2,30 @@ package com.github.kevin.raft.core;
 
 import com.github.kevin.raft.common.ServerStatus;
 import com.github.kevin.raft.dto.AppendEntriesRequest;
-import com.github.kevin.raft.entity.PersistentState;
-import com.github.kevin.raft.entity.VolatileState;
+import com.github.kevin.raft.dto.AppendEntriesResponse;
+import com.github.kevin.raft.dto.RequestVoteRequest;
+import com.github.kevin.raft.dto.RequestVoteResponse;
+import com.github.kevin.raft.entity.*;
 import com.github.kevin.raft.netty.client.NettyClient;
-import com.github.kevin.raft.netty.common.entity.Request;
+import com.github.kevin.raft.netty.common.constants.MessageTypeEnum;
+import com.github.kevin.raft.netty.common.entity.RaftMessage;
 import com.github.kevin.raft.netty.server.NettyServer;
 import com.github.kevin.raft.utils.NameThreadPoolFactory;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 节点
  */
+@Slf4j
 @Data
 public class RaftNode {
 
@@ -27,7 +34,10 @@ public class RaftNode {
      */
     private final String address;
 
-    private String leaderAddress;
+    /**
+     * leader节点的地址
+     */
+    private String leaderId;
 
     /**
      * 其他节点的地址
@@ -49,7 +59,20 @@ public class RaftNode {
      */
     private final PersistentState persistentState;
 
+    /**
+     * 易失性状态
+     */
     private final VolatileState volatileState;
+
+    /**
+     * 日志模块
+     */
+    private final LogModule logModule;
+
+    /**
+     * 状态机
+     */
+    private final StateMachine stateMachine;
 
     /**
      * 节点当前状态，初始化状态为follower
@@ -62,20 +85,31 @@ public class RaftNode {
      * 上次一心跳时间戳
      */
     private volatile long preHeartBeatTime = 0;
+
+    /**
+     * 上一次选举时间
+     */
+    public volatile long preElectionTime = 0;
+
     /**
      * 心跳间隔基数
      */
-    private final long heartBeatTick = 5 * 100;
+    private final long heartBeatTick = 5 * 10;
+
+    /**
+     * 选举时间间隔基数
+     */
+    public volatile long electionTime = 15 * 10;
 
     private ScheduledExecutorService electionScheduledExecutorService = new ScheduledThreadPoolExecutor(1, new NameThreadPoolFactory("election"));
     private ScheduledExecutorService heartbeatScheduledExecutorService = new ScheduledThreadPoolExecutor(1, new NameThreadPoolFactory("heartbeat"));
 
-    public RaftNode(String address, List<String> otherAddresses, Integer port) {
+    public RaftNode(String address, List<String> otherAddresses, Integer port, Boolean startElection) {
         this.address = address;
         this.otherAddresses = otherAddresses;
         this.consensusModule = new ConsensusModule(this);
         // 启动服务端
-        new NettyServer(port);
+        new NettyServer(port, this);
         // 延迟3秒启动客户端
         try {
             Thread.sleep(10000);
@@ -87,10 +121,14 @@ public class RaftNode {
         otherAddresses.forEach(nettyClient::connect);
         this.persistentState = new PersistentState();
         this.volatileState = new VolatileState(otherAddresses.size() + 1);
+        this.logModule = new LogModule();
+        this.stateMachine = new StateMachine();
         // 开启选举线程
-        electionScheduledExecutorService.scheduleWithFixedDelay(new ElectionThread(), 3000, 500, java.util.concurrent.TimeUnit.MILLISECONDS);
+        if (startElection) {
+            electionScheduledExecutorService.scheduleWithFixedDelay(new ElectionThread(), 3000, 3000, java.util.concurrent.TimeUnit.MILLISECONDS);
+        }
         // 开启心跳线程
-        heartbeatScheduledExecutorService.scheduleAtFixedRate(new HeartBeatThread(), 0, 500, java.util.concurrent.TimeUnit.MILLISECONDS);
+//        heartbeatScheduledExecutorService.scheduleAtFixedRate(new HeartBeatThread(), 0, 50, java.util.concurrent.TimeUnit.MILLISECONDS);
     }
 
     /**
@@ -103,6 +141,108 @@ public class RaftNode {
         @Override
         public void run() {
 
+            try {
+                if (status == ServerStatus.LEADER) {
+                    return;
+                }
+
+                long current = System.currentTimeMillis();
+                // 基于 RAFT 的随机时间,解决冲突.
+                long newElectionTime = electionTime + ThreadLocalRandom.current().nextLong(electionTime);
+                if (current - preElectionTime < newElectionTime) {
+                    return;
+                }
+
+                // 变更当前节点状态
+                status = ServerStatus.CANDIDATE;
+                log.error("Node {} will become CANDIDATE and start election leader, current term : [{}], LastEntry : [{}]",
+                        address, persistentState.getCurrentTerm(), logModule.getLastIndex());
+                // 变更上一次选举时间
+                preElectionTime = System.currentTimeMillis() + newElectionTime;
+
+                // 将当前任期加1
+                persistentState.setCurrentTerm(persistentState.getCurrentTerm() + 1);
+
+                // 推荐自己.
+                persistentState.setVotedFor(address);
+
+                List<CompletableFuture<RequestVoteResponse>> futureArrayList = new ArrayList<>();
+                AtomicInteger successVote = new AtomicInteger(1);
+                for (String otherAddress : otherAddresses) {
+                    Long lastTerm = Optional.ofNullable(logModule.getLast()).map(LogEntry::getTerm).orElse(0L);
+                    RequestVoteRequest requestVoteRequest = RequestVoteRequest.builder().term(persistentState.getCurrentTerm()).candidateId(address).lastLogIndex(logModule.getLastIndex()).lastLogTerm(lastTerm).build();
+                    RaftMessage<RequestVoteRequest> request = RaftMessage.<RequestVoteRequest>builder()
+                            .type(MessageTypeEnum.VOTE_REQUEST)
+                            .data(requestVoteRequest)
+                            .build();
+                    futureArrayList.add(this.sendElection(otherAddress, request, successVote));
+                }
+                // 等待投票结果
+                CompletableFuture<Void> completableFuture = CompletableFuture.allOf(futureArrayList.toArray(new CompletableFuture[futureArrayList.size()]));
+                try {
+                    completableFuture.get(newElectionTime, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                } catch (TimeoutException e) {
+                    throw new RuntimeException(e);
+                }
+                // 获取投票结果
+                int success = successVote.get();
+                log.info("Node {} received votes, success count = {}, current term: {}", address, success, persistentState.getCurrentTerm());
+                // 如果投票期间,有其他服务器发送 appendEntry , 就可能变成 follower ,这时,应该停止.
+                if (status == ServerStatus.FOLLOWER) {
+                    return;
+                }
+                log.info("Node {} received votes, success count = {}", address, success);
+
+                // 判断是否成为leader
+                if (success >= (1 + otherAddresses.size()) / 2) {
+                    log.warn("Node {} become leader successfully, current term: {}", address, persistentState.getCurrentTerm());
+                    status = ServerStatus.LEADER;
+                    // 设置leader地址
+                    leaderId = address;
+                    // 清空投票记录
+                    persistentState.setVotedFor(null);
+                    // 执行成为leader后的操作
+                    //                becomeLeaderToDoThing();
+                } else {
+                    log.error("Node {} failed to become leader, current term: {}, success count: {}", address, persistentState.getCurrentTerm(), success);
+                    // 重新开始下一轮选举
+                    persistentState.setVotedFor(null);
+                }
+                // 再次更新选举时间
+                preElectionTime = System.currentTimeMillis() + newElectionTime;
+            } catch (RuntimeException e) {
+                log.error(e.getMessage(), e);
+            }
+
+        }
+
+        private CompletableFuture<RequestVoteResponse> sendElection(String address, RaftMessage<RequestVoteRequest> request, AtomicInteger success) {
+            // todo 调整为异步方式
+            return Optional.ofNullable(RaftRpcClientContainer.getInstance().getRpcClient(address))
+                    .map(rpcClient -> {
+                        CompletableFuture<RequestVoteResponse> requestVoteResponseCompletableFuture = rpcClient.handleRequestVote(request);
+                        return requestVoteResponseCompletableFuture.whenComplete((requestVoteResponse, throwable) -> {
+                            Boolean voteGranted = requestVoteResponse.getVoteGranted();
+                            if (voteGranted) {
+                                log.info("Node {} received successfully vote from {}, current term: {}", address, request.getData().getCandidateId(), persistentState.getCurrentTerm());
+                                // 投票成功，成功票数增加
+                                success.incrementAndGet();
+                            } else {
+                                // 投票失败，判断是否是因为term过期
+                                Long term = requestVoteResponse.getTerm();
+                                if (term > persistentState.getCurrentTerm()) {
+                                    log.error("Received vote from {} with higher term: {}, current term: {}", request.getData().getCandidateId(), term, persistentState.getCurrentTerm());
+                                    // 如果收到的term比当前节点的term大，则更新当前节点的term
+                                    persistentState.setCurrentTerm(term);
+                                }
+                            }
+                            RequestFutureManager.remove(request.getRequestId());
+                        });
+                    }).orElseGet(() -> CompletableFuture.completedFuture(RequestVoteResponse.fail()));
         }
     }
 
@@ -126,17 +266,37 @@ public class RaftNode {
             if (currentTime - preHeartBeatTime < heartBeatTick) {
                 return;
             }
-
+            AppendEntriesRequest appendEntriesRequest = AppendEntriesRequest.builder().entries(null).leaderId(address).currentTerm(persistentState.getCurrentTerm()).leaderCommit(volatileState.getCommitIndex()).build();
+            // todo 修改此处的requestId的生成方式
+            RaftMessage<AppendEntriesRequest> request = new RaftMessage<>(new Random().nextInt(), MessageTypeEnum.APPEND_ENTRIES_REQUEST, appendEntriesRequest);
             // 向所有的follower节点发送心跳
-            otherAddresses.forEach(this::sendHeartBeat);
+            otherAddresses.forEach(address -> {
+                // 发送心跳
+                this.sendHeartBeat(address, request);
+            });
         }
 
-        private void sendHeartBeat(String address) {
+        private void sendHeartBeat(String address, RaftMessage<AppendEntriesRequest> request) {
+            // todo 调整为异步方式
             Optional.ofNullable(RaftRpcClientContainer.getInstance().getRpcClient(address))
                     .ifPresent(rpcClient -> {
-                        AppendEntriesRequest appendEntriesRequest = AppendEntriesRequest.builder().entries(null).leaderAddress(address).serverAddress(address).currentTerm(persistentState.getCurrentTerm()).leaderCommit(volatileState.getCommitIndex()).build();
-                        Request<AppendEntriesRequest> request = new Request<>(Request.A_ENTRIES, appendEntriesRequest);
-                        rpcClient.send(request);
+                        CompletableFuture<AppendEntriesResponse> appendEntriesResponseCompletableFuture = rpcClient.handleAppendEntries(request);
+                        try {
+                            AppendEntriesResponse appendEntriesResponse = appendEntriesResponseCompletableFuture.get(3000, TimeUnit.MILLISECONDS);
+                            Long term = appendEntriesResponse.getTerm();
+                            if (term > persistentState.getCurrentTerm()) {
+                                log.error("Received heartbeat from {} with higher term: {}, current term: {}", address, term, persistentState.getCurrentTerm());
+                                // 如果收到的term比当前节点的term大，则更新当前节点的term
+                                persistentState.setCurrentTerm(term);
+                                persistentState.setVotedFor(null);
+                                // 更新状态为follower
+                                status = ServerStatus.FOLLOWER;
+                            }
+                        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                            throw new RuntimeException(e);
+                        } finally {
+                            RequestFutureManager.remove(request.getRequestId());
+                        }
                     });
         }
     }
